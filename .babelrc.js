5b0730ca0662b148357d5f513cc33ce18d8c2501
Abdiel Martinez <aabdiel1998@gmail.com>
2020-03-26 10:15:51 -0600

    performance(antd)

--- .babelrc.js
+++ .babelrc.js
@@ -4,11 +4,7 @@ module.exports = (()=>({
 	"plugins": [
 		["babel-plugin-styled-components"],
 		["@babel/plugin-proposal-optional-chaining"],
-		["import", { 
-			libraryName: "antd",
-			libraryDirectory: "es",
-			style: 'css'
-		}],
+		["import", { "libraryName": "antd", libraryDirectory: "es", "style": 'css'}],
 		["module-resolver", {
 			"alias": aliasPath
 		}]
